cf6a1c83844d94a900ef9566c0de12f5182e4b59
Aaron Nolan <anolan23@yahoo.com>
2022-01-08 22:10:16 -0600

    add breeds routes

--- backend/routes/breeds.js
+++ backend/routes/breeds.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const catWiki = require('../cat-wiki');
+const db = require('../db/index.js');
 const router = express.Router();
 
 router.get('/api/breeds', async (req, res) => {
@@ -25,6 +26,48 @@ router.get('/api/breeds/search', async (req, res) => {
       },
     });
     res.send(response.data);
+  } catch (error) {
+    res.status(error.status || 500).send({ error: error.message });
+  }
+});
+
+router.post('/api/breeds/search', async (req, res) => {
+  try {
+    const { query } = req.body;
+    await db.query(
+      `
+        INSERT INTO searches (query)
+        VALUES ($1)
+    `,
+      [query]
+    );
+    res.status(200).send();
+  } catch (error) {
+    res.status(error.status || 500).send({ error: error.message });
+  }
+});
+
+router.get('/api/breeds/trending', async (req, res) => {
+  try {
+    const { limit } = req.query;
+    const { rows } = await db.query(
+      `
+      SELECT query, count(*) as search_count
+      FROM searches
+      GROUP BY query
+      ORDER BY search_count DESC
+      OFFSET 0
+      LIMIT $1
+    `,
+      [limit]
+    );
+    const trending = rows.map((search) => search.query);
+    const response = await catWiki.get('/breeds');
+    const breeds = response.data.filter((breed) =>
+      trending.includes(breed.name)
+    );
+
+    res.send(breeds);
   } catch (error) {
     res.status(error.status || 500).send({ error: error.message });
   }
