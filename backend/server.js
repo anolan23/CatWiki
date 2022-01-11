0becbf2c69ed64fa0e506b709ee5e57109c9f81b
Aaron Nolan <anolan23@yahoo.com>
2022-01-11 14:40:37 -0600

    update server and package.json to allow serving of static files from my server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const breedsRouter = require('./routes/breeds.js');
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 8080;
+const htmlPath = path.join(__dirname, '..', 'build', 'index.html');
 
+app.get('/', (req, res) => {
+  res.sendFile(htmlPath);
+});
 app.use(breedsRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
